0bb09b20471955c0a911b45420a1201a1e0b0115
Justin Falcone <stanulam59@gmail.com>
2016-11-21 08:29:30 -0500

    docco

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
+// [Types](types.html)
 export {
     types, createType,
     namespace, addFields, composeModifiers,
 } from "./types"
+// [Actions](actions.html)
 export { createActions } from "./actions"
+// [Reducers](reducers.html)
+export { createReducerCreator } from "./reducers"
